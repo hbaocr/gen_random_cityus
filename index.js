6384c90a21918c8f088fcd604bd9d464b3b0474f
Duong Huynh Bao <duonghuynhbaocr@gmail.com>
2019-11-08 19:04:02 +0700

    not finish analyse yet

--- index.js
+++ index.js
@@ -2,8 +2,50 @@
 let fs = require('fs');
 let gen_src = require('./data_src');
 
-function gen_data(number_data=100,file_name="test-dat.json"){
-    let arr_obj=[];
+function json_obj(prop, val){
+     this.prop=val;
+}
+
+
+function analyse_gender(obj,store){
+    if(store[obj.gender]==undefined){
+        store[obj.gender]=1;
+    }else{
+        store[obj.gender]= store[obj.gender]+1;
+    }
+    return store;
+}
+
+function analyse_age(obj,store){
+    let field=obj.age_range.desc;
+    if(store[field]==undefined){
+        store[field]=1;
+    }else{
+        store[field]= store[field]+1;
+    }
+    return store;
+}
+
+function analyse_product(obj,store){
+    let field=obj.scan.desc;
+    if(store[field]==undefined){
+        store[field]=1;
+    }else{
+        store[field]= store[field]+1;
+    }
+    return store;
+}
+
+let arr_obj=[];
+let gender_inf=[];
+let age_range_inf=[];
+let product_inf=[];
+
+
+function gen_data(number_data=10,file_name="test-dat.json"){
+ 
+  
+
     for(let i=0;i<number_data;i++){
         let obj ={
             gender: gen_src.get_gender(),
@@ -11,12 +53,18 @@ function gen_data(number_data=100,file_name="test-dat.json"){
             scan: gen_src.get_beer_product(),
             geo: gen_src.get_state(),
         }
+        gender_inf=analyse_gender(obj,gender_inf);
+        age_range_inf= analyse_age(obj,age_range_inf);
+        product_inf = analyse_product(obj,product_inf);
         arr_obj.push(obj);
     }
 
     let ret ={
         data: arr_obj,
-        total:arr_obj.length, 
+        total:arr_obj.length,
+        gender_inf: gender_inf,
+        age_range_inf: age_range_inf,
+        product_inf:product_inf
     }
     let str = JSON.stringify(ret);
 
